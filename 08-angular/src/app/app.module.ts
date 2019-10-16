4de9f47b636da2d420b3c1e5ffe151cdd80006ea
Darwin Felipe Uzcategui Portatil Lenovo <darwin.uzcategui1973@hotmail.com>
2019-10-16 13:56:30 -0400

    angular encabezado

--- 08-angular/src/app/app.module.ts
+++ 08-angular/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { MiComponente } from './components/mi-componente/mi-componente.components';
 import { VendedorComponent } from './components/vendedor/vendedor.component';
 import { TestComponent } from './components/test/test.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiComponente,
     VendedorComponent,
-    TestComponent
+    TestComponent,
+    EncabezadoComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
